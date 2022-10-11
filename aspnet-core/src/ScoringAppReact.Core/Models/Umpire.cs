32034f24c4c83a48f923c9677226032f299fa835
humayoun11 <humayounkhatri@gmail.com>
2022-10-11 17:39:43 +0500

    umpire-module-fixes

---
+++ aspnet-core/src/ScoringAppReact.Core/Models/Umpire.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScoringAppReact.Models
+{
+    public class Umpire
+        : FullAuditedEntity<long>
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Contact { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+        public int Status { get; set; }
+
+
+    }
+}
